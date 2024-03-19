2c8ee3c52599dcc942e8f29ba5c3ce0d68a175a5
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-19 11:21:38 +0300

    master branch

--- test_product.py
+++ test_product.py
@@ -7,6 +7,9 @@ class Testproducts(unittest.TestCase):
     def test_product_negative_quantity(self):
         with self.assertRaises(ValueError):
             products = Product('Apple', 10, -5)
+    def test_product_negative_quantity(self):
+        products = Product('Apple', 10, -5)
+        self.assertRaises(ValueError, products.calculate)
             
     def test_productsTotal_zero(self):
         products = Product('Apple', 10, 0)
